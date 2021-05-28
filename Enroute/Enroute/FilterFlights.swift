50d23c608f24b7862cb9257c8bcbe9520f8038fb
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-28 10:48:17 +0900

    [#22] Init

---
+++ Enroute/Enroute/FilterFlights.swift
@@ -0,0 +1,73 @@
+//
+//  FilterFlights.swift
+//  Enroute
+//
+//  Created by Geonhyeong LIm on 2021/05/28.
+//  Copyright © 2021 Stanford University. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilterFlights: View {
+    @ObservedObject var allAirports = Airports.all
+    @ObservedObject var allAirlines = Airlines.all
+    
+    @Binding var flightSearch: FlightSearch
+    @Binding var isPresented: Bool
+    
+    @State private var draft: FlightSearch
+    
+    init(flightSearch: Binding<FlightSearch>, isPresented: Binding<Bool>) {
+        _flightSearch = flightSearch
+        _isPresented = isPresented
+        _draft = State(wrappedValue: flightSearch.wrappedValue)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Destination", selection: $draft.destination, content: {
+                    ForEach(allAirports.codes, id: \.self) { airport in
+                        Text("\(self.allAirports[airport]?.friendlyName ?? airport)")
+                    }
+                })
+                Picker("Origin", selection: $draft.origin, content: {
+                    Text("Any").tag(String?.none)
+                    ForEach(allAirports.codes, id: \.self) { (airport: String?) in
+                        Text("\(self.allAirports[airport]?.friendlyName ?? airport ?? "Any")").tag(airport)
+                    }
+                })
+                Picker("Airline", selection: $draft.airline, content: {
+                    Text("Any").tag(String?.none)
+                    ForEach(allAirlines.codes, id: \.self) { (airline: String?) in
+                        Text("\(self.allAirlines[airline]?.friendlyName ?? airline ?? "Any")").tag(airline)
+                    }
+                })
+                Toggle(isOn: $draft.inTheAir) {
+                    Text("Enroute Only")
+                }
+            }
+            .navigationBarTitle("Filter Flights")
+            .navigationBarItems(leading: cancel, trailing: done)
+        }
+    }
+    
+    var cancel: some View {
+        Button("Cancel") {
+            self.isPresented = false
+        }
+    }
+    
+    var done: some View {
+        Button("Done") {
+            self.flightSearch = self.draft
+            self.isPresented = false
+        }
+    }
+}
+
+//struct FilterFlights_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FilterFlights()
+//    }
+//}
